296a7380a0a55e2cc7a40fa953a8a3f1babbc771
Pepper <imadori_wow@hotmail.com>
2015-05-15 13:39:23 -0500

    Core changes to index.html, main.css, main.min.js

--- laravel-basics/public/js/main.js
+++ laravel-basics/public/js/main.js
@@ -1,146 +1,3 @@
-/* ===============
-   CAPTTION FOR CYCLE2
-   =============== */
-
-(function($) {
-"use strict";
-
-$.extend($.fn.cycle.defaults, {
-    captionFxOut:      'fadeOut',
-    captionFxIn:       'fadeIn',
-    captionFxSel:      undefined,
-    overlayFxOut:      'fadeOut',
-    overlayFxIn:       'fadeIn',
-    overlayFxSel:      undefined
-});    
-
-$(document).on( 'cycle-bootstrap', function(e, opts) {
-    opts.container.on( 'cycle-update-view-before', update );
-    opts.container.one( 'cycle-update-view-after', init );
-});
-
-// $(document).on( 'cycle-destroy', function(e, opts) {
-//     opts.container.off( 'cycle-update-view-before', update );
-// });
-
-function update( e, opts, slideOpts, currSlide, isAfter ) {
-    if ( opts.captionPlugin !== 'caption2' )
-        return;
-    $.each(['caption','overlay'], function() {
-        var fxBase = this + 'Fx',
-            fx = opts[fxBase + 'Out'] || 'hide',
-            template = slideOpts[this+'Template'],
-            el = opts.API.getComponent( this ),
-            sel = opts[fxBase+'Sel'],
-            speed = opts.speed,
-            animEl;
-
-        if ( opts.sync )
-            speed = speed/2;
-
-        animEl = sel ? el.find( sel ) : el;
-
-        if( el.length && template ) {
-            if ( fx == 'hide')
-                speed = 0;
-            animEl[fx]( speed, function() {
-                var content = opts.API.tmpl( template, slideOpts, opts, currSlide );
-                el.html( content );
-                animEl = sel ? el.find( sel ) : el;
-                if ( sel )
-                    animEl.hide();
-                fx = opts[ fxBase + 'In'] || 'show';
-                animEl[fx]( speed );
-            });
-        }
-        else {
-            el.hide();
-        }
-    });
-}
-
-function init( e, opts, slideOpts, currSlide, isAfter ) {
-    if ( opts.captionPlugin !== 'caption2' )
-        return;
-    $.each(['caption','overlay'], function() {
-        var template = slideOpts[this+'Template'],
-            el = opts.API.getComponent( this );
-
-        if( el.length && template )
-            el.html( opts.API.tmpl( template, slideOpts, opts, currSlide ) );
-    });
-}
-
-})(jQuery);
-
-/* ===============
-   CENTER PLUGIN CYCLE2
-   =============== */
-
-(function($) {
-"use strict";
-
-$.extend($.fn.cycle.defaults, {
-    centerHorz: false,
-    centerVert: false
-});
-
-$(document).on( 'cycle-pre-initialize', function( e, opts ) {
-    if ( !opts.centerHorz && !opts.centerVert )
-        return;
-
-    // throttle resize event
-    var timeout, timeout2;
-
-    $(window).on( 'resize orientationchange load', resize );
-    
-    opts.container.on( 'cycle-destroyed', destroy );
-
-    opts.container.on( 'cycle-initialized cycle-slide-added cycle-slide-removed', function( e, opts, slideOpts, slide ) {
-        resize();
-    });
-
-    adjustActive();
-
-    function resize() {
-        clearTimeout( timeout );
-        timeout = setTimeout( adjustActive, 50 );
-    }
-
-    function destroy( e, opts ) {
-        clearTimeout( timeout );
-        clearTimeout( timeout2 );
-        $( window ).off( 'resize orientationchange', resize );
-    }
-
-    function adjustAll() {
-        opts.slides.each( adjustSlide ); 
-    }
-
-    function adjustActive() {
-        /*jshint validthis: true */
-        adjustSlide.apply( opts.container.find( '.' + opts.slideActiveClass ) );
-        clearTimeout( timeout2 );
-        timeout2 = setTimeout( adjustAll, 50 );
-    }
-
-    function adjustSlide() {
-        /*jshint validthis: true */
-        var slide = $(this);
-        var contW = opts.container.width();
-        var contH = opts.container.height();
-        var w = slide.outerWidth();
-        var h = slide.outerHeight();
-        if (w) {
-            if (opts.centerHorz && w <= contW)
-                slide.css( 'marginLeft', (contW - w) / 2 );
-            if (opts.centerVert && h <= contH)
-                slide.css( 'marginTop', (contH - h) / 2 );
-        }
-    }
-});
-
-})(jQuery);
 
 /* ===============
    CAROUSEL
@@ -207,25 +64,35 @@ $('.mobile-menu').click(function(){
   } else {
     $(".menu-icon use").attr('xlink:href', '#menu');
   }
-  $('.login').removeClass('mostrar-menu');
+  $('.login').removeClass('mostrar-menu clicked');
 });
 
 
-$('#profile-photo').click(function(){
+$( "#profile-photo" ).click(function(){
   var navClase = $('.login');
   var size = $(window).width();
-  // si tiene slidein usar slideout
   if (size<1024) {
-    $('.login').toggleClass('mostrar-menu');
-    $('.login').addClass('slideInLeft');
-    $('.login').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-     $('.login').removeClass('slideInLeft');
-    });
+      if ($('.login').hasClass('clicked')) {
+        $('.login').addClass('slideOutLeft');
+        $('.login').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+          $('.login').removeClass('slideOutLeft');
+          $('.login').removeClass('clicked');
+          $('.login').removeClass('mostrar-menu');
+        });
+      } else {
+          $('.login').addClass('slideInLeft');
+          $('.login').addClass('mostrar-menu');
+          $('.login').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+          $('.login').removeClass('slideInLeft');
+          $('.login').addClass('clicked');
+        });
+      };
     $('nav').removeClass('mostrar-menu');
     $('.mobile-menu').removeClass('active');
     $(".menu-icon use").attr('xlink:href', '#menu');
   };
 });
+
 
 $('#login-button').click(function(){
   $('.overlay').show();
@@ -258,7 +125,7 @@ $(window).resize(function(){
 
 // HOVER MENU DESKTOP
 
-$( ".login-menu-button" )
+$( "#profile-photo" )
   .mouseenter(function() {
     var size = $(window).width();
     if (size>1024) {
